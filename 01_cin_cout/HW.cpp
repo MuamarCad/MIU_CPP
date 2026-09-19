5926afc211f77114a4634bba9e2aa50113e08caa
lophandm <3staskupr@gmail.com>
2026-09-19 18:13:41 +0300

    update(HW.cpp): Добавлено решение задания 3

--- 01_cin_cout/HW.cpp
+++ 01_cin_cout/HW.cpp
@@ -4,17 +4,19 @@ using namespace std;
 
 //Объявление функций (задач)
 //void task_1();//1 A+B
-void task_2();//Сколько тебе лет?
+//void task_2();//Сколько тебе лет?
+void task_3();//Билеты
 
 int main () {
 
     //Вызов функций(задач)
     //task_1(); //1 A+B
-    task_2();//Сколько тебе лет?
+    //task_2();//Сколько тебе лет?
+    task_3();//Билеты
 
     return 0;
 }
-
+/*
 void task_1() {//1 A+B
     int a = 0;
     int b = 0;
@@ -27,8 +29,8 @@ void task_1() {//1 A+B
     int result = a + b;
 
     cout << "Ответ: " << result << "\n";
-}
-
+}*/
+/*
 void task_2(){;//Сколько тебе лет?
     int b = 0;
     int n = 0;
@@ -39,6 +41,23 @@ void task_2(){;//Сколько тебе лет?
     cin >> n;
     
     int result = n - b;
+
+    cout << "Ответ: " << result << "\n";
+
+}*/
+
+void task_3(){;//Билеты
+    int s = 0;
+    int a = 0;
+
+    cout << "Введите общую стоимость: ";
+    cin >> s;
+    cout << "Введите стоимость взрослого билета: ";
+    cin >> a;
+    
+    int count = a * 3;
+
+    int result = s - count;
 
     cout << "Ответ: " << result << "\n";
 
