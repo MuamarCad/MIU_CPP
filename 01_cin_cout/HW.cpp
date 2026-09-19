aa66fae883a99c62b2bdb1f5b9e586128c97c8de
lophandm <3staskupr@gmail.com>
2026-09-19 16:58:58 +0300

    update(HW.cpp): Добавлено решение задания 2

--- 01_cin_cout/HW.cpp
+++ 01_cin_cout/HW.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 //Объявление функций (задач)
-void task_1();//1 A+B
+//void task_1();//1 A+B
+void task_2();//Сколько тебе лет?
 
 int main () {
 
     //Вызов функций(задач)
-    task_1(); //1 A+B
+    //task_1(); //1 A+B
+    task_2();//Сколько тебе лет?
 
     return 0;
 }
@@ -25,4 +27,19 @@ void task_1() {//1 A+B
     int result = a + b;
 
     cout << "Ответ: " << result << "\n";
+}
+
+void task_2(){;//Сколько тебе лет?
+    int b = 0;
+    int n = 0;
+
+    cout << "Введите год рождения: ";
+    cin >> b;
+    cout << "Введите текущий год: ";
+    cin >> n;
+    
+    int result = n - b;
+
+    cout << "Ответ: " << result << "\n";
+
 }
